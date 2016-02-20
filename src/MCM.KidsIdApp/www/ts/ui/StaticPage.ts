e85788cd685c3501e008bcc8029fc02a0d1b3992
David Brown <DavidBr@magenic.net>
2016-02-20 14:36:36 -0600

    update pre-pull

--- src/MCM.KidsIdApp/www/ts/ui/StaticPage.ts
+++ src/MCM.KidsIdApp/www/ts/ui/StaticPage.ts
@@ -2,19 +2,23 @@ module MCM{
   export class StaticPageCtrl{
     public static $inject="$scope";
 
-    constructor($scope){
+    public scope:any;
 
+    constructor($scope){
+      this.scope = $scope;
+      console.log("CONTROLLER MO FO", this.scope.pageReference)
     }
   }
 
   export function StaticPageDir():any{
     return {
-      scope: {'pageReference': '=ref'},
+      scope: {'pageReference': '@ref'},
       restrict: "E",
       link: (scope, ele, attrs, controllers, transcludeFn)=> {
 
       },
-      template: "<div>This Is A Test</div>"
+      template: "<div>This Is A Test</div>",
+      controller:StaticPageCtrl
     }
   }
 }
