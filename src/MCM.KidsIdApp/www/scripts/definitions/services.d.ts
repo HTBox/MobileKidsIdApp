1cc75d2254f301b9743a972450f06a5fe616da77
Nate Dahlquist <ncdahlquist@yahoo.com>
2016-02-21 15:04:02 -0600

    #79 create FileSystem service for persisting data using the cordova File plugin.

---
+++ src/MCM.KidsIdApp/www/scripts/definitions/services.d.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="../definitions/angular.d.ts" />
+
+declare module MCM {
+
+    interface IFileService {
+
+        saveTextToFile(text: string): angular.IPromise<void>
+        getFileText(): angular.IPromise<string>
+    }
+
+
+}
