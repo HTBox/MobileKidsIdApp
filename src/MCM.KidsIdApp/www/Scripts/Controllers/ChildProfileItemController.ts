b6fcc5022b508321265fc566f87b78224943cf54
Esten Rye <esten@ryezone.net>
2016-02-21 04:43:05 -0600

    #4 - Build out Child Profile Item Menu

---
+++ src/MCM.KidsIdApp/www/Scripts/Controllers/ChildProfileItemController.ts
@@ -0,0 +1,47 @@
+﻿/// <reference path="../Definitions/angular.d.ts" />
+/// <reference path="../models/models.ts" />
+/// <reference path="IControllerNavigation.ts" />
+
+class ChildProfileItemController implements IControllerNavigation {
+    private _state;
+    private _scope;
+    private _navigationLinks: Array<NavigationLink>;
+
+    constructor($scope, $state) {
+        this._state = $state;
+        this._scope = $scope;
+
+        this._navigationLinks =
+            [
+                new NavigationLink("addPhoto", "Add Photo", "ion-ios-person-outline", "ion-chevron-right"),
+                new NavigationLink("childBasics", "Child Basics", "ion-android-person", "ion-chevron-right"),
+                new NavigationLink("measurements", "Measurements", "ion-android-calendar", "ion-chevron-right"),
+                new NavigationLink("physicalDetails", "Physical Details", "ion-ios-eye", "ion-chevron-right"),
+                new NavigationLink("doctorInfo", "Doctor Info", "ion-network", "ion-chevron-right"),
+                new NavigationLink("dentalInfo", "Dental Info", "ion-android-happy", "ion-chevron-right"),
+                new NavigationLink("medicalAlertInfo", "Medical Alert Info", "ion-medkit", "ion-chevron-right"),
+                new NavigationLink("distinguishingFeatures", "Distinguishing Features", "ion-ios-body", "ion-chevron-right"),
+                new NavigationLink("idChecklist", "I.D. Checklist", "ion-checkmark", "ion-chevron-right")
+            ];
+    }
+
+    public static $inject = ["$scope", "$state"]
+
+    public NavigateToHomeScreen() {
+        this.NavigateTo('landing');
+    }
+
+    public NavigateToPreviousView() {
+        this.NavigateTo('childProfileList');
+    }
+
+    public NavigationLinks() {
+        return this._navigationLinks;
+    }
+
+    public NavigateTo($controller: string) {
+        this._state.go($controller);
+    }
+}
+
+angular.module('mcmapp').controller('childProfileItemController', ChildProfileItemController);
--- src/MCM.KidsIdApp/www/Scripts/Controllers/IControllerNavigation.ts
+++ src/MCM.KidsIdApp/www/Scripts/Controllers/IControllerNavigation.ts
@@ -1,11 +1,15 @@
 ﻿class NavigationLink {
-    constructor($controllerTarget: string, $linkText: string) {
+    constructor($controllerTarget: string, $linkText: string, $leftIcon: string = "", $rightIcon: string = "") {
         this.ControllerTarget = $controllerTarget;
         this.LinkText = $linkText;
+        this.LeftIcon = $leftIcon;
+        this.RightIcon = $rightIcon;
     }
 
     ControllerTarget: string;
     LinkText: string;
+    LeftIcon: string;
+    RightIcon: string;
 }
 
 interface IControllerNavigation {
