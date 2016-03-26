62af544a2bd53d4abd74de10b4e9a39c3647cbe3
Dan Nordquist <dan@dnord.com>
2016-03-26 12:07:24 -0500

    new empty service for email functionality

---
+++ src/MCM.KidsIdApp.MobileApi/Controllers/EmailController.cs
@@ -0,0 +1,20 @@
+﻿using MCM.KidsIdApp.MobileApp.DataObjects;
+using Microsoft.Azure.Mobile.Server.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MCM.KidsIdApp.MobileApp.Controllers
+{
+    [MobileAppController]
+    public class EmailController : ApiController
+    {
+        // POST: api/Email
+        public void Post([FromBody]EmailRequest value)
+        {
+        }
+    }
+}
---
+++ src/MCM.KidsIdApp.MobileApi/DataObjects/EmailRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MCM.KidsIdApp.MobileApp.DataObjects
+{
+    public class EmailRequest
+    {
+        public int Id { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
