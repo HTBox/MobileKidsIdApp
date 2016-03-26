bca4b699ed0393938ef860a4d37d72122d0a6a90
Dan Nordquist <dan@dnord.com>
2016-03-26 13:34:05 -0500

    sends an email
    not a pretty one, but, y'know
    
    - we did not need a model. it's all dynamic anyway.
    - added SendGrid to the party.

--- src/MCM.KidsIdApp.MobileApi/Controllers/EmailController.cs
+++ src/MCM.KidsIdApp.MobileApi/Controllers/EmailController.cs
@@ -1,11 +1,14 @@
 ﻿using MCM.KidsIdApp.MobileApp.DataObjects;
 using Microsoft.Azure.Mobile.Server.Config;
+using SendGrid;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Mail;
 using System.Web.Http;
+using System.Threading.Tasks;
 
 namespace MCM.KidsIdApp.MobileApp.Controllers
 {
@@ -13,8 +16,15 @@ namespace MCM.KidsIdApp.MobileApp.Controllers
     public class EmailController : ApiController
     {
         // POST: api/Email
-        public void Post([FromBody]EmailRequest value)
         {
+            var myMessage = new SendGridMessage();
+            myMessage.AddTo(value.EmailAddress.ToString());
+            myMessage.From = new MailAddress("dan@dnord.com", "Dan Nordquist");
+            myMessage.Subject = "Your Details";
+            myMessage.Text = value.Profile.ToString();
+
+            var transportWeb = new Web("key goes here");
+            transportWeb.DeliverAsync(myMessage).Wait();
         }
     }
 }
