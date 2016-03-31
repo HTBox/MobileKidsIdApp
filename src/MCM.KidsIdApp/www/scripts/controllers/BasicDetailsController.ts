ea74493bf89fcc801350cba85048f651195b3e7d
Nate Dahlquist <ncdahlquist@yahoo.com>
2016-03-30 22:36:57 -0500

    #5 Add functionality to basic details screen for linking to a phone contact.

--- src/MCM.KidsIdApp/www/scripts/controllers/BasicDetailsController.ts
+++ src/MCM.KidsIdApp/www/scripts/controllers/BasicDetailsController.ts
@@ -10,31 +10,34 @@ module MCM {
     export class BasicDetailsController {
 
         //private _scope: any;
-        private _state: angular.ui.IStateService;
-        private _ionicPopup: ionic.popup.IonicPopupService;
-        private _childDataService: MCM.ChildDataService;
 
-        public static $inject = ['$scope', '$state', '$stateParams', '$ionicPopup', 'childDataService'];
+        public static $inject = ['$scope', '$state', '$stateParams', '$ionicScrollDelegate',
+                '$ionicPopup', 'childDataService', 'contactsService'];
 
-        constructor($scope: ng.IScope, $state: angular.ui.IStateService, $stateParams: any,
-                $ionicPopup: ionic.popup.IonicPopupService, childDataService: MCM.ChildDataService) {
+        constructor($scope: ng.IScope, private $state: angular.ui.IStateService, $stateParams: any,
+                private $ionicScrollDelegate: ionic.scroll.IonicScrollDelegate,
+                private $ionicPopup: ionic.popup.IonicPopupService,
+                private childDataService: MCM.ChildDataService,
+                private contactsService: MCM.ContactsService) {
             //this._scope = $scope;
-            this._state = $state;
-            this._ionicPopup = $ionicPopup;
             let childId = $stateParams.childId;
             childDataService.getById(childId).then(child => {
                 const childDetails = (child && child.childDetails) ?
                         angular.copy(child.childDetails) : <ChildDetails>{ givenName: "", familyName: "" };
                 this.doDatePickerSetup(childDetails.birthday || new Date());
+                const contactId = childDetails.contact ? childDetails.contact.contactId : null;
+                contactsService.findContactById(contactId).then(contact => {
+                    this.populatePhoneContactInfoFromContact(contact);
+                });
                 this.childDetails = childDetails;
             });
             this._childId = childId;
-            this._childDataService = childDataService;
             this.doDatePickerSetup(null);
         }
         
         private _childId: string
         public childDetails: ChildDetails;
+        public phoneContact: Contact;
         public datepickerObject;
 
         public checkChildDetailsHasChanges(editedChildDetails: ChildDetails,
@@ -44,17 +47,28 @@ module MCM {
         }
 
         public NavigateToPreviousView() {
-            let hasChangesPromise = this._childDataService.getById(this._childId)
-                .then(chld => this.checkChildDetailsHasChanges(this.childDetails, chld.childDetails));
+            let childChangeInfoPromise = this.childDataService.getById(this._childId)
+                .then(chld => {
+                    const hasChanges = !chld || this.checkChildDetailsHasChanges(this.childDetails, chld.childDetails);
+                    return { isChildPersisted: chld != null, hasChanges };
+                });
             
-            hasChangesPromise.then(hasChanges => {
-                let go = () => this._state.go("childProfileItem", { childId: this._childId });
-                if (hasChanges) {
-                    this._ionicPopup.confirm({
+            childChangeInfoPromise.then(childChangeInfo => {
+                let go = () => this.$state.go("childProfileItem", { childId: this._childId });
+                if (childChangeInfo.hasChanges) {
+                    this.$ionicPopup.confirm({
                         title: 'Confirm Leave Page',
                         template: 'There are unsaved changes. Ignore changes and leave?'
                     }).then(answer => {
-                        if (answer) { go(); }
+                        if (answer) {
+                            if (childChangeInfo.isChildPersisted) {
+                                go();
+                            } else {
+                                //If leaving the page and the child has never been persisted, go all the
+                                //way back to child profile list.
+                                this.$state.go("childProfileList");
+                            }
+                        }
                     });
                 } else {
                     go();
@@ -62,14 +76,42 @@ module MCM {
             });
         }
 
+        private populatePhoneContactInfoFromContact(contact: Contact) {
+            this.phoneContact = contact;
+            //Calling resize here was an attempt at fixing an issue on a Android 4.3 phone where content
+            //gets cut off at the bottom of the screen, but not sure that it helped anything.
+            const doResize = this.$ionicScrollDelegate.resize();
+            setTimeout(doResize, 500);
+        }
+
+        public linkToContact() {
+            return this.contactsService.pickContact().then(contact => {
+                this.childDetails.contact = {
+                    contactId: contact.id
+                };
+                if (contact.name) {
+                    if (!this.childDetails.givenName && contact.name.givenName) {
+                        this.childDetails.givenName = contact.name.givenName;
+                    }
+                    if (!this.childDetails.familyName && contact.name.familyName) {
+                        this.childDetails.familyName = contact.name.familyName;
+                    }
+                    if (!this.childDetails.additionalName && contact.name.middleName) {
+                        this.childDetails.additionalName = contact.name.middleName;
+                    }
+                }
+                this.populatePhoneContactInfoFromContact(contact);
+            });
+        }
+
         public saveChanges(formValid: boolean) {
             if (formValid) {
-                return this._childDataService.getById(this._childId).then(child => {
+                return this.childDataService.getById(this._childId).then(child => {
                     if (!child) {
                         child = <Child>{ id: this._childId };
                     }
                     child.childDetails = this.childDetails;
-                    return this._childDataService.update(child);
+                    return this.childDataService.update(child);
                 });
             }
         }
@@ -84,16 +126,8 @@ module MCM {
                 callback: (newDate => { this.childDetails.birthday = newDate; }).bind(this),
             };
         }
-        
-        public test() {
-            navigator.contacts.pickContact(function (contact) {
-                console.log('The following contact has been selected:' + JSON.stringify(contact));
-            }, function (err) {
-                console.log('Error: ' + err);
-            });
-    }
 
-}
+    }
 }
 
 angular.module('mcmapp').controller('basicDetailsController', MCM.BasicDetailsController);
