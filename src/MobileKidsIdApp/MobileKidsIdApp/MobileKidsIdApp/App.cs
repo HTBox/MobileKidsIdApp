25e2d2dca03f308f6db13f8fdadc1eef1bcd6bce
Rockford Lhotka <rocky@lhotka.net>
2016-05-07 10:41:56 -0500

    #133 Add basic landing page

--- src/MobileKidsIdApp/MobileKidsIdApp/MobileKidsIdApp/App.cs
+++ src/MobileKidsIdApp/MobileKidsIdApp/MobileKidsIdApp/App.cs
@@ -12,19 +12,7 @@ namespace MobileKidsIdApp
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new LandingPage();
         }
 
         protected override void OnStart()
