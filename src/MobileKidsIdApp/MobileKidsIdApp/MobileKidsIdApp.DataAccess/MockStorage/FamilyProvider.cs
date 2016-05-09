c2a533e9e53a56c3e3f9b676b99d22d2431442a1
Rockford Lhotka <rocky@lhotka.net>
2016-05-09 15:40:00 -0500

    #133 Create mock in-memory database

---
+++ src/MobileKidsIdApp/MobileKidsIdApp/MobileKidsIdApp.DataAccess/MockStorage/FamilyProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MobileKidsIdApp.DataAccess.DataModels;
+using System.Threading.Tasks;
+using PCLStorage;
+using Newtonsoft.Json;
+
+namespace MobileKidsIdApp.DataAccess.MockStorage
+{
+    public class FamilyProvider : IFamilyProvider
+    {
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public async Task<Family> Get()
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        {
+            return MockDb.Family;
+        }
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public async Task Save(Family data)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        {
+            MockDb.Family = data;
+        }
+    }
+}
