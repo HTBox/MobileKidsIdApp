c881cfc13188a7f8bbc565d80423b0fd28789ad6
Brian Seebacher <brian@haiint.com>
2016-08-29 09:38:57 -0500

    WIP iOS Contact Picker implmentation.

--- src/MobileKidsIdApp/MobileKidsIdApp/MobileKidsIdApp.iOS/Services/ContactPicker.cs
+++ src/MobileKidsIdApp/MobileKidsIdApp/MobileKidsIdApp.iOS/Services/ContactPicker.cs
@@ -5,18 +5,64 @@ using System.Linq;
 using System.Text;
 using MobileKidsIdApp.ViewModels;
 using System.Threading.Tasks;
-
-using Plugin.Contacts;
-using Plugin.Contacts.Abstractions;
+using ContactsUI;
+using Contacts;
+using Foundation;
+using UIKit;
 
 [assembly: Xamarin.Forms.Dependency(typeof(MobileKidsIdApp.iOS.Services.ContactPicker))]
 namespace MobileKidsIdApp.iOS.Services
 {
+    /// <summary>
+    /// Implements IContactPicker service for iOS using MonoTouch framework support.
+    /// </summary>
+    /// <remarks>
+    /// See https://developer.xamarin.com/recipes/ios/shared_resources/contacts/choose_a_contact/
+    /// </remarks>
     public class ContactPicker :  IContactPicker
     {
+
+        protected ContactInfo SelectedContactInfo { get; set; }
+
         public async Task<ContactInfo> GetSelectedContactInfo()
         {
-            throw new NotImplementedException();
+           
+            var tcs = new TaskCompletionSource<ContactInfo>();
+
+            var picker = new CNContactPickerViewController();
+
+            // Select property to pick
+            picker.DisplayedPropertyKeys = new NSString[] { CNContactKey.GivenName, CNContactKey.FamilyName };
+
+            // Respond to selection
+            var pickerDelegate = new ContactPickerDelegate();
+            picker.Delegate = pickerDelegate;
+
+            pickerDelegate.ContactSelected += (contact) => {
+                SelectedContactInfo = new ContactInfo { Id = contact.Identifier, DisplayName = string.Format("{0} {1}", contact.GivenName, contact.FamilyName) };
+            };
+
+            UIWindow window = UIApplication.SharedApplication.KeyWindow;
+            UIViewController viewController = window.RootViewController;
+            if (viewController == null)
+            {
+                while (viewController.PresentedViewController != null)
+                    viewController = viewController.PresentedViewController;
+
+            }
+
+            viewController.PresentViewController(picker, true, null);
+
+
+            return SelectedContactInfo;
+            // Display picker
+
+   
+            
         }
+
+
     }
+
+
 }
---
+++ src/MobileKidsIdApp/MobileKidsIdApp/MobileKidsIdApp.iOS/Services/ContactPickerController.cs
@@ -0,0 +1,120 @@
+﻿using Contacts;
+using ContactsUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UIKit;
+
+namespace MobileKidsIdApp.iOS.Services
+{
+    public sealed class ContactPickerController : CNContactPickerViewController
+    {
+        internal ContactPickerController(ContactPickerDelegate cpDelegate)
+        {
+            base.Delegate = cpDelegate;
+        }
+        public override ICNContactPickerDelegate Delegate
+        {
+            get
+            {
+                return base.Delegate;
+            }
+
+            set
+            {
+                base.Delegate = value;
+            }
+        }
+
+    //    public Task<MediaFile> GetResultAsync() =>
+    //((MediaPickerDelegate)Delegate).Task;
+    }
+
+    //public class ContactPickerDelegate : CNContactPickerDelegate
+    //{
+    //    #region Constructors
+    //    public ContactPickerDelegate()
+    //    {
+    //    }
+
+    //    public ContactPickerDelegate(IntPtr handle) : base(handle)
+    //    {
+    //    }
+    //    #endregion
+
+    //    #region Override Methods
+    //    public override void ContactPickerDidCancel(CNContactPickerViewController picker)
+    //    {
+    //        // Raise the selection canceled event
+    //        RaiseSelectionCanceled();
+    //    }
+
+    //    public override void DidSelectContact(CNContactPickerViewController picker, CNContact contact)
+    //    {
+    //        // Raise the contact selected event
+    //        RaiseContactSelected(contact);
+    //    }
+
+    //    public override void DidSelectContactProperty(CNContactPickerViewController picker, CNContactProperty contactProperty)
+    //    {
+    //        // Raise the contact property selected event
+    //        RaiseContactPropertySelected(contactProperty);
+    //    }
+    //    #endregion
+
+    //    #region Events
+    //    public delegate void SelectionCanceledDelegate();
+    //    public event SelectionCanceledDelegate SelectionCanceled;
+
+    //    internal void RaiseSelectionCanceled()
+    //    {
+    //        if (this.SelectionCanceled != null) this.SelectionCanceled();
+    //    }
+
+    //    public delegate void ContactSelectedDelegate(CNContact contact);
+    //    public event ContactSelectedDelegate ContactSelected;
+
+    //    internal void RaiseContactSelected(CNContact contact)
+    //    {
+    //        if (this.ContactSelected != null) this.ContactSelected(contact);
+    //    }
+
+    //    public delegate void ContactPropertySelectedDelegate(CNContactProperty property);
+    //    public event ContactPropertySelectedDelegate ContactPropertySelected;
+
+    //    internal void RaiseContactPropertySelected(CNContactProperty property)
+    //    {
+    //        if (this.ContactPropertySelected != null) this.ContactPropertySelected(property);
+    //    }
+    //    #endregion
+    //}
+
+
+    //    internal MediaPickerController(MediaPickerDelegate mpDelegate)
+    //    {
+    //        base.Delegate = mpDelegate;
+    //    }
+
+    //    /// <summary>
+    //    /// Deleage
+    //    /// </summary>
+    //    public override NSObject Delegate
+    //    {
+    //        get { return base.Delegate; }
+    //        set
+    //        {
+    //            if (value == null)
+    //                base.Delegate = value;
+    //            else
+    //                throw new NotSupportedException();
+    //        }
+    //    }
+
+    //    /// <summary>
+    //    /// Gets result of picker
+    //    /// </summary>
+    //    /// <returns></returns>
+    //    public Task<MediaFile> GetResultAsync() =>
+    //        ((MediaPickerDelegate)Delegate).Task;
+    //}
+}
