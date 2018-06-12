26688b7a8c4ddda7363f1fbd2ff14a5c87dc1c8f
Brian Seebacher <bseebacher@haiint.com>
2018-06-11 20:24:37 -0500

    WIP static file import implementation.

---
+++ src/MobileKidsIdApp/MobileKidsIdApp.iOS/Services/WebViewContentHelper_IOS.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Foundation;
+
+using MobileKidsIdApp.Services;
+
+[assembly: Xamarin.Forms.Dependency(typeof(MobileKidsIdApp.iOS.Services.WebViewContentHelper_IOS))]
+namespace MobileKidsIdApp.iOS.Services
+{
+    public class WebViewContentHelper_IOS : IWebViewContentHelper
+    {
+        public string GetBaseUrl()
+        {
+            return NSBundle.MainBundle.BundlePath;
+        }
+
+
+        public string LoadContentString(string contentResourceName)
+        {
+            var assembly = IntrospectionExtensions
+                .GetTypeInfo(typeof(MobileKidsIdApp.App)).Assembly;
+            Stream stream = assembly.GetManifestResourceStream("MobileKidsIdApp.Resources.abduction.html");
+            string text = "";
+            using (var reader = new System.IO.StreamReader(stream))
+            {
+                text = reader.ReadToEnd();
+            }
+            return text;
+            //string path = CreatePathToFile(filename);
+            //using (var sr = File.OpenText(path))
+            //{
+            //    return await sr.ReadToEndAsync();
+            //}
+        }
+    }
+}
--- src/MobileKidsIdApp/MobileKidsIdApp/Services/IBaseUrl.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-namespace MobileKidsIdApp.Services
-{
-    public interface IHtmlContentLoader
-    {
-    }
-}
