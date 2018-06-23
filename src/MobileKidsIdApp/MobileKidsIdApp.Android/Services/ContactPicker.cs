10d7b25edd579bf8bac695de996a4e1365c1d7c8
Brian Runck <brunck@users.noreply.github.com>
2018-06-23 17:57:10 -0400

    Issue #226 - fix Contact Picker on Android (#245)

--- src/MobileKidsIdApp/MobileKidsIdApp.Android/Services/ContactPicker.cs
+++ src/MobileKidsIdApp/MobileKidsIdApp.Android/Services/ContactPicker.cs
@@ -1,76 +1,81 @@
+using System.Threading.Tasks;
+using Android.App;
+using MobileKidsIdApp.Services;
+using MobileKidsIdApp.ViewModels;
+using Android.Provider;
+using Android.Content;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using MobileKidsIdApp.Services;
-using MobileKidsIdApp.ViewModels;
-//using Plugin.Contacts.Abstractions;
-//using Plugin.Contacts;
-using Android.Provider;
 
 [assembly: Xamarin.Forms.Dependency(typeof(MobileKidsIdApp.Droid.Services.ContactPicker))]
 namespace MobileKidsIdApp.Droid.Services
-{     
+{
     public class ContactPicker : Java.Lang.Object, IContactPicker
     {
         public Task<ContactInfo> GetSelectedContactInfo()
         {
             var tcs = new TaskCompletionSource<ContactInfo>();
-            //TODO: Update to replace no-longer-supported plugin
-            //Intent pickContactIntent =
-            //    new Intent(Intent.ActionPick, Android.Net.Uri.Parse("content://contacts"));
-            //pickContactIntent.SetType(Android.Provider.ContactsContract.CommonDataKinds.Phone.ContentType); // Show user only contacts w/ phone numbers
 
-            //var handler = new EventHandler<ActivityResultEventArgs>((sender, e) => OnActivityResult(tcs, e));
-            //MainActivity.Instance.ActivityResult += handler;
-            //tcs.Task.ContinueWith(t => MainActivity.Instance.ActivityResult -= handler);
-            //try
-            //{
-            //    MainActivity.Instance.StartActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
-            //}
-            //catch (Exception ex)
-            //{
-            //    tcs.SetException(ex);
-            //}
+            Intent pickContactIntent =
+                new Intent(Intent.ActionPick, Android.Net.Uri.Parse("content://contacts"));
+            pickContactIntent.SetType(Android.Provider.ContactsContract.CommonDataKinds.Phone.ContentType); // Show user only contacts w/ phone numbers
+
+            var handler = new EventHandler<ActivityResultEventArgs>((sender, e) => OnActivityResult(tcs, e));
+            MainActivity.Instance.ShowContactPicker += handler;
+            tcs.Task.ContinueWith(t => MainActivity.Instance.ShowContactPicker -= handler);
+            try
+            {
+                MainActivity.Instance.StartActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+            }
             return tcs.Task;
         }
         
         static int PICK_CONTACT_REQUEST = 42; // The request code
-        
-        protected void OnActivityResult(TaskCompletionSource<ContactInfo> tcs, ActivityResultEventArgs e) //int requestCode, Android.App.Result resultCode, Intent data)
-        {
-            //TODO: Update to replace no-longer-supported plugin
-            //// Check which request it is that we're responding to
-            //if (e.requestCode == PICK_CONTACT_REQUEST)
-            //{
-            //    // Make sure the request was successful
-            //    if (e.resultCode == Android.App.Result.Ok)
-            //    {
-            //        var loader = new CursorLoader(MainActivity.Instance, e.data.Data, projection, null, null, null);
-            //        var cursor = (Android.Database.ICursor)loader.LoadInBackground();
 
-            //        var contactList = new List<ContactInfo>();
-            //        if (cursor.MoveToFirst())
-            //        {
-            //            do
-            //            {
-            //                contactList.Add(GetContactInfoFromCursor(cursor));
-            //            } while (cursor.MoveToNext());
-            //        }
-            //        tcs.SetResult(contactList.FirstOrDefault());
-            //        return;
-            //    }
-            //}
+        static Activity CurrentActivity
+        {
+            get
+            {
+                return MainActivity.Instance;
+            }
+        }
+
+        public static bool IsShowContactPickerIntent(int requestCode)
+        {
+            return requestCode == PICK_CONTACT_REQUEST;
+        }
+
+        protected void OnActivityResult(TaskCompletionSource<ContactInfo> tcs, ActivityResultEventArgs e)
+         {
+            // Check which request it is that we're responding to
+            if (e.requestCode == PICK_CONTACT_REQUEST)
+            {
+                // Make sure the request was successful
+                if (e.resultCode == Android.App.Result.Ok)
+                {
+                    var loader = new CursorLoader(MainActivity.Instance, e.data.Data, projection, null, null, null);
+                    var cursor = (Android.Database.ICursor)loader.LoadInBackground();
+
+                    var contactList = new List<ContactInfo>();
+                    if (cursor.MoveToFirst())
+                    {
+                        do
+                        {
+                            contactList.Add(GetContactInfoFromCursor(cursor));
+                        } while (cursor.MoveToNext());
+                    }
+                    tcs.SetResult(contactList.FirstOrDefault());
+                    return;
+                }
+            }
 
             tcs.SetResult(null);
-        }
+         }
 
         private ContactInfo GetContactInfoFromCursor(Android.Database.ICursor cursor)
         {
