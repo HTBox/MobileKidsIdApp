4775129146722e4520462e468cd8e80d19123d34
Rockford Lhotka <rocky@lhotka.net>
2018-06-22 21:49:35 -0500

    Closes #231 Restore use of PCLCrypto

--- src/MobileKidsIdApp/MobileKidsIdApp.Android/Resources/Resource.Designer.cs
+++ src/MobileKidsIdApp/MobileKidsIdApp.Android/Resources/Resource.Designer.cs
@@ -26,6 +26,8 @@ namespace MobileKidsIdApp.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::PCLCrypto.Resource.String.ApplicationName = global::MobileKidsIdApp.Droid.Resource.String.ApplicationName;
+			global::PCLCrypto.Resource.String.Hello = global::MobileKidsIdApp.Droid.Resource.String.Hello;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::MobileKidsIdApp.Droid.Resource.Attribute.actionBarSize;
 		}
 		
@@ -3961,6 +3963,12 @@ namespace MobileKidsIdApp.Droid
 		
 		public partial class String
 		{
+			
+			// aapt resource value: 0x7f0a003d
+			public const int ApplicationName = 2131361853;
+			
+			// aapt resource value: 0x7f0a003c
+			public const int Hello = 2131361852;
 			
 			// aapt resource value: 0x7f0a0015
 			public const int abc_action_bar_home_description = 2131361813;
