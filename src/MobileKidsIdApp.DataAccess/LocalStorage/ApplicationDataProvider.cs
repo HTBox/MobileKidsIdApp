e056f72adfc9f199e45f22b950327f389dada2e8
Rockford Lhotka <rocky@lhotka.net>
2018-06-22 23:00:07 -0500

    #218 Implement file snapshot/backup functionality for Family data. #229 Implement Get/Save methods using System.IO.IsolatedStorage.

--- src/MobileKidsIdApp.DataAccess/LocalStorage/ApplicationDataProvider.cs
+++ src/MobileKidsIdApp.DataAccess/LocalStorage/ApplicationDataProvider.cs
@@ -2,41 +2,62 @@
 using MobileKidsIdApp.DataAccess.DataModels;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.IO.IsolatedStorage;
+using System.IO;
 
 namespace MobileKidsIdApp.DataAccess.LocalStorage
 {
     public class ApplicationDataProvider : IApplicationDataProvider
     {
+        private static readonly string FileName = "ApplicationData.txt";
+        private static readonly string BackupFileName = "ApplicationData.bak";
+        private static readonly IsolatedStorageScope FileScope = IsolatedStorageScope.User | IsolatedStorageScope.Assembly | IsolatedStorageScope.Roaming;
+
         public async Task<ApplicationData> Get()
         {
             ApplicationData result = null;
-            //TODO: rewrite to use System.IO.File
-            //var fileSystem = FileSystem.Current;
-            //var rootFolder = fileSystem.LocalStorage;
-            //var file = await rootFolder.GetFileAsync("ApplicationData.txt");
-            //if (file != null)
-            //{
-            //    await file.OpenAsync(FileAccess.Read);
-            //    var dataBlob = await file.ReadAllTextAsync();
-            //    var json = Encryption.Decrypt(Csla.ApplicationContext.User.Identity.Name, dataBlob);
-            //    result = JsonConvert.DeserializeObject<ApplicationData>(json);
-            //}
-            //else
-            //{
-            //    result = new ApplicationData();
-            //}
+            using (var isoStore = IsolatedStorageFile.GetStore(FileScope, null))
+            {
+                if (isoStore.FileExists(FileName))
+                {
+                    using (var isoStream = new IsolatedStorageFileStream(FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        using (var reader = new StreamReader(isoStream))
+                        {
+                            var json = await reader.ReadLineAsync();
+                            var dataBlob = Encryption.Decrypt(Csla.ApplicationContext.User.Identity.Name, json);
+                            result = JsonConvert.DeserializeObject<ApplicationData>(dataBlob);
+                        }
+                    }
+                }
+                else
+                {
+                    result = new ApplicationData();
+                }
+            }
             return result;
         }
 
         public async Task Save(ApplicationData data)
         {
-            //TODO: rewrite to use System.IO.File
-            //var json = JsonConvert.SerializeObject(data);
-            //var dataBlob = Encryption.Encrypt(Csla.ApplicationContext.User.Identity.Name, json);
-            //var fileSystem = FileSystem.Current;
-            //var rootFolder = fileSystem.LocalStorage;
-            //var file = await rootFolder.CreateFileAsync("ApplicationData.txt", CreationCollisionOption.ReplaceExisting);
-            //await file.WriteAllTextAsync(dataBlob);
+            using (var isoStore = IsolatedStorageFile.GetStore(FileScope, null))
+            {
+                if (isoStore.FileExists(FileName))
+                {
+                    if (isoStore.FileExists(BackupFileName))
+                        isoStore.DeleteFile(BackupFileName);
+                    isoStore.CopyFile(FileName, BackupFileName);
+                }
+                using (var isoStream = new IsolatedStorageFileStream(FileName, FileMode.Create, FileAccess.Write))
+                {
+                    using (var writer = new StreamWriter(isoStream))
+                    {
+                        var json = JsonConvert.SerializeObject(data);
+                        var dataBlob = Encryption.Encrypt(Csla.ApplicationContext.User.Identity.Name, json);
+                        await writer.WriteLineAsync(dataBlob);
+                    }
+                }
+            }
         }
     }
 }
--- src/MobileKidsIdApp.DataAccess/LocalStorage/FamilyProvider.cs
+++ src/MobileKidsIdApp.DataAccess/LocalStorage/FamilyProvider.cs
@@ -2,40 +2,62 @@
 using MobileKidsIdApp.DataAccess.DataModels;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.IO.IsolatedStorage;
+using System.IO;
 
 namespace MobileKidsIdApp.DataAccess.LocalStorage
 {
     public class FamilyProvider : IFamilyProvider
     {
+        private static readonly string FileName = "Family.txt";
+        private static readonly string BackupFileName = "Family.bak";
+        private static readonly IsolatedStorageScope FileScope = IsolatedStorageScope.User | IsolatedStorageScope.Assembly | IsolatedStorageScope.Roaming;
+
         public async Task<Family> Get()
         {
             Family result = null;
-            //TODO: rewrite to use System.IO.File
-            //var fileSystem = FileSystem.Current;
-            //var rootFolder = fileSystem.LocalStorage;
-            //var file = await rootFolder.GetFileAsync("Family.txt");
-            //if (file != null)
-            //{
-            //    await file.OpenAsync(FileAccess.Read);
-            //    var json = await file.ReadAllTextAsync();
-            //    result = JsonConvert.DeserializeObject<Family>(json);
-            //}
-            //else
-            //{
-            //    result = new Family();
-            //}
+            using (var isoStore = IsolatedStorageFile.GetStore(FileScope, null))
+            {
+                if (isoStore.FileExists(FileName))
+                {
+                    using (var isoStream = new IsolatedStorageFileStream(FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        using (var reader = new StreamReader(isoStream))
+                        {
+                            var json = await reader.ReadLineAsync();
+                            var dataBlob = Encryption.Decrypt(Csla.ApplicationContext.User.Identity.Name, json);
+                            result = JsonConvert.DeserializeObject<Family>(dataBlob);
+                        }
+                    }
+                }
+                else
+                {
+                    result = new Family();
+                }
+            }
             return result;
         }
 
         public async Task Save(Family data)
         {
-            //TODO: rewrite to use System.IO.File
-            //var json = JsonConvert.SerializeObject(data);
-            //var dataBlob = Encryption.Encrypt(Csla.ApplicationContext.User.Identity.Name, json);
-            //var fileSystem = FileSystem.Current;
-            //var rootFolder = fileSystem.LocalStorage;
-            //var file = await rootFolder.CreateFileAsync("Family.txt", CreationCollisionOption.ReplaceExisting);
-            //await file.WriteAllTextAsync(json);
+            using (var isoStore = IsolatedStorageFile.GetStore(FileScope, null))
+            {
+                if (isoStore.FileExists(FileName))
+                {
+                    if (isoStore.FileExists(BackupFileName))
+                        isoStore.DeleteFile(BackupFileName);
+                    isoStore.CopyFile(FileName, BackupFileName);
+                }
+                using (var isoStream = new IsolatedStorageFileStream(FileName, FileMode.Create, FileAccess.Write))
+                {
+                    using (var writer = new StreamWriter(isoStream))
+                    {
+                        var json = JsonConvert.SerializeObject(data);
+                        var dataBlob = Encryption.Encrypt(Csla.ApplicationContext.User.Identity.Name, json);
+                        await writer.WriteLineAsync(dataBlob);
+                    }
+                }
+            }
         }
     }
 }
