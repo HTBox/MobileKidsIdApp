d30264fc93c365b4c74919044b4f0a61b792cf50
Brian Runck <Brian.Runck@bd.com>
2018-08-04 14:36:17 -0400

    package name and related changes

--- src/MobileKidsIdApp/MobileKidsIdApp.iOS/Properties/AssemblyInfo.cs
+++ src/MobileKidsIdApp/MobileKidsIdApp.iOS/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MobileKidsIdApp.iOS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Missing Children Minnesota Kids Id Kit")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Humanitarian Toolbox and Missing Children Minnesota")]
 [assembly: AssemblyProduct("MobileKidsIdApp.iOS")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
