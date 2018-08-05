a2a9309a9d9cf17141ddd41b4c30b6350098f367
Jon Cwiak <binaryjanitor@outlook.com>
2018-08-05 14:37:19 -0500

    PDF rendering in app

--- src/MobileKidsIdApp/MobileKidsIdApp.UWP/CustomWebViewRenderer.cs
+++ src/MobileKidsIdApp/MobileKidsIdApp.UWP/CustomWebViewRenderer.cs
@@ -1,11 +1,11 @@
 ﻿using MobileKidsIdApp.UWP;
-using MobileKidsIdApp.Views;
+using MobileKidsIdApp.Controls;
 using System;
 using System.Net;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.UWP;
 
-[assembly: ExportRenderer(typeof(DocumentRender), typeof(CustomWebViewRenderer))]
+[assembly: ExportRenderer(typeof(DocumentWebView), typeof(CustomWebViewRenderer))]
 namespace MobileKidsIdApp.UWP
 {
     public class CustomWebViewRenderer : WebViewRenderer
@@ -16,7 +16,7 @@ namespace MobileKidsIdApp.UWP
 
             if (e.NewElement != null)
             {
-                var customWebView = Element as Controls.DocumentWebView;
+                var customWebView = Element as DocumentWebView;
                 Control.Source = new Uri(string.Format("ms-appx-web:///Assets/pdfjs/web/viewer.html?file={0}", string.Format("ms-appx-web:///Assets/Content/{0}", WebUtility.UrlEncode(customWebView.Uri))));
             }
         }
