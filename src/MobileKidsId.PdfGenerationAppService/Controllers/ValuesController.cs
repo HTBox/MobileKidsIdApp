a49fa94fb3f13954881cdefa93ebdf707239f1f4
Jon Cwiak <binaryjanitor@outlook.com>
2018-08-05 10:12:16 -0500

    Add PdfGenerationAppService

---
+++ src/MobileKidsId.PdfGenerationAppService/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+
+namespace MobileKidsId.PdfGenerationAppService.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [SwaggerOperation("GetAll")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [SwaggerOperation("GetById")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [SwaggerOperation("Create")]
+        [SwaggerResponse(HttpStatusCode.Created)]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [SwaggerOperation("Update")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [SwaggerOperation("Delete")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public void Delete(int id)
+        {
+        }
+    }
+}
