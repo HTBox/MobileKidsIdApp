b75c4eec67ccf6155d27d57ac1309870c9aaa99e
binaryjanitor <binaryjanitor@outlook.com>
2020-08-04 17:00:14 -0500

    Refactor to service

---
+++ src/MobileKidsIdApp/MobileKidsIdApp/Services/DocumentGenerationService.cs
@@ -0,0 +1,81 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MobileKidsIdApp.Services
+{
+    public class DocumentGenerationService
+    {
+        public string GeneratePdfDcoumentFromTemplate(string template)
+        {
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), $"{Guid.NewGuid():N}.pdf");
+            GenerateDocument(path);
+            return path;
+        }
+
+        private void GenerateDocument(string path)
+        {
+            var metadata = new SKDocumentPdfMetadata
+            {
+                Author = "GiveCamp 2020",
+                Creation = DateTime.Now,
+                Creator = "That Conference Library",
+                Keywords = "GiveCamp, That Conference, Test",
+                Modified = DateTime.Now,
+                Producer = "Test",
+                Subject = "Test",
+                Title = "Test PDF",
+            };
+            using var document = SKDocument.CreatePdf(path, metadata);
+            using var paint = new SKPaint
+            {
+                TextSize = 64.0f,
+                IsAntialias = true,
+                Color = 0xFF9CAFB7,
+                IsStroke = true,
+                StrokeWidth = 3,
+                TextAlign = SKTextAlign.Center
+            };
+            var pageWidth = 840;
+            var pageHeight = 1188;
+
+            // draw page 1
+            using (var pdfCanvas = document.BeginPage(pageWidth, pageHeight))
+            {
+                // draw button
+                using var nextPagePaint = new SKPaint
+                {
+                    IsAntialias = true,
+                    TextSize = 16,
+                    Color = SKColors.OrangeRed
+                };
+                var nextText = "Next Page >>";
+                var btn = new SKRect(pageWidth - nextPagePaint.MeasureText(nextText) - 24, 0, pageWidth, nextPagePaint.TextSize + 24);
+                pdfCanvas.DrawText(nextText, btn.Left + 12, btn.Bottom - 12, nextPagePaint);
+                // make button link
+                pdfCanvas.DrawLinkDestinationAnnotation(btn, "next-page");
+
+                // draw contents
+                pdfCanvas.DrawText("...PDF 1/2...", pageWidth / 2, pageHeight / 4, paint);
+
+                document.EndPage();
+            }
+            // draw page 2
+            using (var pdfCanvas = document.BeginPage(pageWidth, pageHeight))
+            {
+                // draw link destintion
+                pdfCanvas.DrawNamedDestinationAnnotation(SKPoint.Empty, "next-page");
+
+                // draw contents
+                pdfCanvas.DrawText("...PDF 2/2...", pageWidth / 2, pageHeight / 4, paint);
+                document.EndPage();
+            }
+
+            // end the doc
+            document.Close();
+            
+        } 
+    }  
+}
--- src/MobileKidsIdApp/MobileKidsIdApp/Views/DocumentsPage.xaml.cs
+++ src/MobileKidsIdApp/MobileKidsIdApp/Views/DocumentsPage.xaml.cs
@@ -1,80 +1,16 @@
-﻿using SkiaSharp;
+﻿using MobileKidsIdApp.Services;
 using System;
 using System.IO;
-using Xamarin.Forms;
 
 namespace MobileKidsIdApp.Views
 {
     public partial class DocumentsPage : ContentPageBase
     {
         public DocumentsPage()
-        { 
-            InitializeComponent();
-            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), $"{Guid.NewGuid():N}.pdf");
-            GenerateDocument(path);
-            customWebView.Uri = path;  
-        }
-     
-
-        private void GenerateDocument(string path)
         {
-            var metadata = new SKDocumentPdfMetadata
-            {
-                Author = "GiveCamp 2020",
-                Creation = DateTime.Now,
-                Creator = "That Conference Library",
-                Keywords = "GiveCamp, That Conference, Test",
-                Modified = DateTime.Now,
-                Producer = "Test",
-                Subject = "Test",
-                Title = "Test PDF",
-            };
-            using var document = SKDocument.CreatePdf(path, metadata);
-            using var paint = new SKPaint
-            {
-                TextSize = 64.0f,
-                IsAntialias = true,
-                Color = 0xFF9CAFB7,
-                IsStroke = true,
-                StrokeWidth = 3,
-                TextAlign = SKTextAlign.Center
-            };
-            var pageWidth = 840;
-            var pageHeight = 1188;
-
-            // draw page 1
-            using (var pdfCanvas = document.BeginPage(pageWidth, pageHeight))
-            {
-                // draw button
-                using var nextPagePaint = new SKPaint
-                {
-                    IsAntialias = true,
-                    TextSize = 16,
-                    Color = SKColors.OrangeRed
-                };
-                var nextText = "Next Page >>";
-                var btn = new SKRect(pageWidth - nextPagePaint.MeasureText(nextText) - 24, 0, pageWidth, nextPagePaint.TextSize + 24);
-                pdfCanvas.DrawText(nextText, btn.Left + 12, btn.Bottom - 12, nextPagePaint);
-                // make button link
-                pdfCanvas.DrawLinkDestinationAnnotation(btn, "next-page");
-
-                // draw contents
-                pdfCanvas.DrawText("...PDF 1/2...", pageWidth / 2, pageHeight / 4, paint);
-                document.EndPage();
-            }
-            // draw page 2
-            using (var pdfCanvas = document.BeginPage(pageWidth, pageHeight))
-            {
-                // draw link destintion
-                pdfCanvas.DrawNamedDestinationAnnotation(SKPoint.Empty, "next-page");
-
-                // draw contents
-                pdfCanvas.DrawText("...PDF 2/2...", pageWidth / 2, pageHeight / 4, paint);
-                document.EndPage();
-            }
-
-            // end the doc
-            document.Close();
-        }    }
-
+            InitializeComponent();
+            var dgs = new DocumentGenerationService();
+            customWebView.Uri = dgs.GeneratePdfDcoumentFromTemplate("Missing Child");
+        }
+    }
 }
